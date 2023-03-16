eac00b0a6305db8fd5ef31aaa47e99b90e2b492e
Luyinq <85760533+Luyinq@users.noreply.github.com>
2023-03-16 11:05:55 -0300

    16-03-2023 11:10
    API base creada

--- API/PTitulo/models.py
+++ API/PTitulo/models.py
@@ -2,3 +2,9 @@ from django.db import models
 from model_utils.models import TimeStampedModel, SoftDeletableModel
 
 
+class Article(models.Model):
+    title = models.CharField(max_length=250)
+    body = models.CharField(max_length=500)
+
+    def __str__(self):
+        return self.title
